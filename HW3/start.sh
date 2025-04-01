11251414115847c98eb239c63dc9baea7eb5cb60
Le Quang Tri <le.quang.tri.official@gmail.com>
2025-04-01 09:41:26 -0400

    Update dockerfile, bash script

--- HW3/start.sh
+++ HW3/start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-echo "⏳ Waiting for Postgres..."
-while ! nc -z db 5432; do
-  sleep 0.5
+# Wait for Postgres to be available
+echo "Waiting for PostgreSQL..."
+while ! nc -z postgres-db 5432; do
+  sleep 1
 done
-echo "✅ Postgres is up!"
 
-echo "🗂 Running Alembic migrations..."
+echo "PostgreSQL started"
+
+# Run Alembic migrations
 alembic upgrade head
 
-echo "🚀 Starting FastAPI server..."
-exec uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+# Start FastAPI app
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
